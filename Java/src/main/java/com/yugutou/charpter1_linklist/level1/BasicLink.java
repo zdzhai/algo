71548416bd1fa11802f0e155e69586ecc29389ef
zzd <zhaizhidong2021@163.com>
2023-10-16 22:30:59 +0800

    BasicLink

--- Java/src/main/java/com/yugutou/charpter1_linklist/level1/BasicLink.java
+++ Java/src/main/java/com/yugutou/charpter1_linklist/level1/BasicLink.java
@@ -37,6 +37,80 @@ public class BasicLink {
         }
     }
 
+    /**
+     * 链表尾部添加节点
+     * @param head 原始链表头节点
+     * @param newNode 新增节点
+     */
+    public static void addNodeInEnd(Node head, Node newNode) {
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = newNode;
+    }
 
+    /**
+     * 链表头部添加节点
+     * @param head 原始链表头节点
+     * @param newNode 新增节点
+     */
+    public static Node addNodeInHead(Node head, Node newNode) {
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    /**
+     * 链表中间添加节点
+     * @param head
+     * @param val
+     * @param newNode
+     * @return
+     */
+    public static Node addNodeInMid(Node head, int val, Node newNode) {
+        Node node = head;
+        while (node.val != val) {
+            node = node.next;
+        }
+        /*Node temp = node.next;
+        node.next = newNode;
+        newNode.next = temp;*/
+        newNode.next = node.next;
+        node.next = newNode;
+        return head;
+    }
+
+    /**
+     * 链表删除节点
+     * @param head 原始链表头节点
+     * @param removeVal 要删除的节点
+     */
+    public static void removeNode(Node head, int removeVal) {
+        if (head == null) {
+            return;
+        }
+        Node node = head;
+        while (node.next.val != removeVal) {
+            node = node.next;
+        }
+        node.next = node.next.next;
+    }
+
+    class DoubleNode {
+        private int val;
+        private DoubleNode left;
+        private DoubleNode right;
+
+        public DoubleNode(int val) {
+            this.val = val;
+        }
+
+        public DoubleNode(int val, DoubleNode left, DoubleNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
 }
